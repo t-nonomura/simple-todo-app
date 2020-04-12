1fe731c280cab3898fe7462f1e18021d78f14dc2
t-nonomura <nono.develop@gmail.com>
2020-04-13 06:39:42 +0900

    完了済みToDoに取り消し線を付ける

--- app/src/main/java/com/treeengineering/simpletodoapp/ui/todo/ToDoListItem.kt
+++ app/src/main/java/com/treeengineering/simpletodoapp/ui/todo/ToDoListItem.kt
@@ -1,5 +1,6 @@
 package com.treeengineering.simpletodoapp.ui.todo
 
+import android.graphics.Paint
 import com.treeengineering.simpletodoapp.R
 import com.treeengineering.simpletodoapp.data.db.entity.ToDo
 import com.treeengineering.simpletodoapp.databinding.ItemTodoListBinding
@@ -18,6 +19,14 @@ class ToDoListItem(
     override fun getLayout() = R.layout.item_todo_list
 
     override fun bind(viewBinding: ItemTodoListBinding, position: Int) {
+        // 完了済みのToDoには取り消し線を付ける
+        val paint = viewBinding.textTodoTitle.paint
+        if (toDo.completed) {
+            paint.flags = viewBinding.textTodoTitle.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+            paint.isAntiAlias = true
+        } else {
+            paint.flags = viewBinding.textTodoTitle.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
+        }
         viewBinding.textTodoTitle.text = toDo.title
         viewBinding.checkboxTodo.isChecked = toDo.completed
         viewBinding.cardViewTodo.setOnClickListener {
